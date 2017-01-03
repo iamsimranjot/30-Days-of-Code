411116f593a11787617d3a24c9a0d3597784163c
Simranjot Singh <iamsimranjot@simranjot-2.local>
2017-01-03 15:11:07 +0530

    Solution Day0 (Swift)

---
+++ Day0/Solution.swift
@@ -0,0 +1,15 @@
+//
+//  Solution.swift
+//  
+//
+//  Created by Simranjot Singh on 03/01/17.
+//
+//
+
+let inputString = readLine()! // get a line of input from stdin and save it to our variable
+
+// Your first line of output goes here
+print("Hello, World.")
+
+// Write the second line of output
+print(inputString)
