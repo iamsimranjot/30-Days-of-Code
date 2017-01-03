9ea859664944cac5ac20a65d8565f87fe863e9ea
Simranjot Singh <iamsimranjot@simranjot-2.local>
2017-01-03 15:19:03 +0530

    Solution Day1 (Swift)

---
+++ Day1/Solution.swift
@@ -0,0 +1,31 @@
+//
+//  Solution.swift
+//  
+//
+//  Created by Simranjot Singh on 03/01/17.
+//
+//
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var i2 : Int
+var j2 : Double
+var s2 : String
+
+// Read and save an integer, double, and String to your variables.
+i2 = Int(readLine()!)!
+j2 = Double(readLine()!)!
+s2 = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i + i2)
+
+// Print the sum of the double variables on a new line.
+print(d + j2)
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s + s2)
