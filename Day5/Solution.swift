6cb06a4454319362f266f629d77255a564a49dbb
Simranjot Singh <iamsimranjot@simranjot-2.local>
2017-01-04 12:49:37 +0530

    Solution Day5 (Swift)

---
+++ Day5/Solution.swift
@@ -0,0 +1,14 @@
+//
+//  Solution.swift
+//  
+//
+//  Created by Simranjot Singh on 04/01/17.
+//
+//
+
+//Read Input
+let n = Int(readLine()!)!
+
+for i in 1 ... 10 {
+    print("\(n) x \(i) = \(n * i)");
+}
